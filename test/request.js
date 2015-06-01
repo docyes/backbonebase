d7d34d5baecf3e0ba72a51693a43a0d20b5d5ee7
Carl Yestrau <carl@featureblend.com>
2015-05-31 23:10:28 -0700

    add wiring in setup/teardown for fake XHR

--- test/request.js
+++ test/request.js
@@ -1,13 +1,23 @@
 (function() {
     module('BackboneBase.Request', {
         setup: function() {
+            var requests = this.requests = [];
             this.Model = Backbone.Model.extend({});
             _.extend(this.Model.prototype, BackboneBase.Request);
+            this.xhr = sinon.useFakeXMLHttpRequest();  
+            this.xhr.onCreate = function(xhr) {
+                requests.push(xhr);
+            };
+        },
+        
+        teardown: function() {
+            this.xhr.restore();
         }
     });
 
     test('stub', 1, function() {
         ok(true, 'worked');
     });
+
 })();
 
