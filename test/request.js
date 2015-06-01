5b2089485e7738dd20079c87f4e4cb6a32811de4
Carl Yestrau <carl@featureblend.com>
2015-05-31 22:29:04 -0700

    add stub for request unit tests

---
+++ test/request.js
@@ -0,0 +1,11 @@
+(function() {
+    module('BackboneBase.Request', {
+        setup: function() {
+        }
+    });
+
+    test('stub', 1, function() {
+        ok(true, 'worked');
+    });
+})();
+
